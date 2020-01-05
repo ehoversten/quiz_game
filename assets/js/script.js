3e5d1855f64919b53288a0382ab916ca83458f6a
ehoversten <ehoversten@gmail.com>
2020-01-04 19:09:31 -0500

    added button listener, button click adds temp text representing a question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 // Testing connection
 console.log("scripts loaded");
 
+// Let's connect our button
+let btn = document.getElementById("start");
+btn.addEventListener("click", startGame);
+
+// Let's connect to our question div
+let questionDiv = document.getElementById("question");
+
+
+function startGame() {
+    console.log("clicked");
+
+    // Create a new element to hold our question
+    let displayQuestion = document.createElement("h1");
+    // Add text to our element
+    displayQuestion.textContent = "Button Clicked!";
+    // Add question to the DOM
+    questionDiv.appendChild(displayQuestion);
+}
+
+
+
 
