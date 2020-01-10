f41caa6390f96c6cde7ef89b59608b4d9897a5e6
ehoversten <ehoversten@gmail.com>
2020-01-09 21:58:26 -0500

    tracking down game play errors and fixing logic mistakes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ console.log("scripts loaded");
 
 //  Declare Global Variables
 // Define amount of game time (in seconds)
-let count = 60;
+let count;
 
 // How many questions do we have?
 let numQuestions = questions.length;
@@ -13,6 +13,8 @@ let currentQuestion;
 
 // Create a variable to hold the STATE of the game
 let gameEnd = true;
+
+// Create a variable to store the current games score
 let gameScore;
 
 // Declare our timer variable globally and we can clear it anywhere in our code later (not just when the timer ends, say when the game ends)
@@ -21,27 +23,27 @@ let timerInterval;
 // Create a GLOBAL variable to hold our users answers
 let userAnswers = [];
 
-// ** TEMP TESTING CODE ** //
-let highScore = 85;
+// Create a variable to PERSIST our high score
+// let highScoreArray = [
+//     {
+//         username: "Bobby",
+//         score: 45
+//     },
+//     {
+//         username: "Manhattan",
+//         score: 42
+//     },
+//     {
+//         username: "Kire",
+//         score: 40
+//     }
+// ];
 
-let highScoreArray = [
-    {
-        username: "Bobby",
-        score: 96
-    },
-    {
-        username: "Manhattan",
-        score: 90
-    },
-    {
-        username: "Kire",
-        score: 89
-    } 
-];
-// ** TEMP TESTING CODE ** //
+// localStorage.setItem("userScores", JSON.stringify(highScoreArray));
 
 // Grab HTML elements for later DOM manipulation
 let score = document.getElementById("score");
+let username = document.getElementById("username");
 let time = document.getElementById("time");
 
 // Let's connect our button
@@ -64,24 +66,61 @@ let scoreBoard = document.getElementById("scoreboard");
 //    and sort through object array for highest score.
 //
 // ---------------------------------------------------- //
-
 function initialize() {
     console.log("Setting up");
 
+    // ** Given Data Set ** /
+    // let dataSetScores = [
+    //     {
+    //         username: "Bobby",
+    //         score: 45
+    //     },
+    //     {
+    //         username: "Manhattan",
+    //         score: 42
+    //     },
+    //     {
+    //         username: "Kire",
+    //         score: 40
+    //     }
+    // ];
+
+    // localStorage.setItem("userScores", JSON.stringify(highScoreArray));
+    // ** TEMP TESTING CODE ABOVE ** //
+
     // Pull out scores from the localStorage OBJECT
-    let topScore = localStorage.getItem("userScore")
+    let findTopScore = localStorage.getItem("userScores");
 
     // Parse the string JSON object into a JavaScript Object
-    let parseScore = JSON.parse(topScore);
-    console.log(parseScore);
+    let parsedScore = JSON.parse(findTopScore);
+    console.log(parsedScore);
+
+    // create a variable to hold the max score;
+    let max = 0;
+    let user;
+
+    // Let's find the TOP score
+    for(let i = 0; i < parsedScore.length; i++) {
+        // TEST to see if current score [i] is greater than current value of max
+        // NOTE that we are dealing with an OBJECT, the OBJECT contains both 'username' and 'score' properties
+        if(max < parsedScore[i].score) {
+            // IF GREATER than set new max value
+            max = parsedScore[i].score;
+            user = parsedScore[i].username;
+        }
+    }
 
     // Update high score in DOM
-    score.textContent = parseScore.score;
+    score.textContent = max;
+    username.textContent = user;
 }
+
+// We declared it let's call it on Browser!
+initialize();
 
 // ---------------------------------------------------- //
 //
-// Start Game Function
+// Start Game Function: 
 //
 // ---------------------------------------------------- //
 function startGame() {
@@ -95,11 +134,19 @@ function startGame() {
     userAnswers = [];
 
     // Initialize Timer
+    // Define amount of game time (in seconds)
+    count = 75;
+    // call timer function to initiate timerInterval
     timer();
+    // Update the timer on the DOM
     time.textContent = count;
 
     // Hide start button
     btn.classList.add("hide");
+    // Hide Leader Board
+    scoreBoard.classList.add("hide");
+    ask.classList.remove("hide");
+
 
     // Run check function
     check();
@@ -126,6 +173,7 @@ function timer() {
         if (count === 0 || gameEnd === true) {
             // Create a new element to hold the time left
             let timeDisplay = document.createElement("p");
+            timeDisplay.setAttribute("id", "timeup");
             // Add text to our element
             timeDisplay.textContent = "Times Up!";
             // Add Element to the DOM
@@ -159,18 +207,19 @@ function check() {
 //
 // ---------------------------------------------------- //
 function next(event) {
-    // event.stopPropagation();
-
+    // Uncomment the lines below and inspect the console output if your struggling with events
     // console.log(event);
     // console.log(event.target);
     console.log(event.target.id);
-    // WHY CAN"T I USE VALUE HERE????
+
     console.log(event.target.innerText);
     userAnswers.push(event.target.innerText);
     
     clear();
     // Increment currentQuestion
     currentQuestion++;
+
+    // Run check
     check();
 }
 
@@ -212,37 +261,46 @@ function loadQuestion() {
 
 // ---------------------------------------------------- //
 //
-// Clear Question Function:
+// Clear Question Function: Clears the question and selection text from the DOM
 //
 // ---------------------------------------------------- //
 function clear() {
     // ** THIS WILL NOT WORK THE WAY WE THINK ** //
+    // This way will remove the HTML elements from the DOM, so after question #1 there is no
+    // DOM ELEMENT to change the text content of.
     // questionDiv.removeChild(askQuestion);
     // questionDiv.removeChild(choices);
 
     // ** THIS WILL NOT WORK THE WAY WE THINK ** //
+    // Same thing as above, removes the DOM NODE
     // askQuestion.remove();
     // choices.remove();
 
-    // ** THIS WILL NOT WORK THE WAY WE THINK ** //
-    // questionDiv.textContent = '';
+    // ** Either of the two methods below will work ** //
     // askQuestion.textContent = '';
+    // choices.textContent = '';
 
-    // questionDiv.innerHTML = '';
+    // We clear out the text contents 
     askQuestion.innerHTML = '';
     choices.innerHTML = '';
+
     return;
 }
 
 // ---------------------------------------------------- //
 //
-// End Game Function:
+// End Game Function: Updates gameEnd variable, clears the timer interval, displays end game text 
+//      calls the scoring function and the username form
 //
 // ---------------------------------------------------- //
 function endGame() {
 
+    // Set gameEnd variable to TRUE
+    gameEnd = true;
+
     // Clear countdown timer
     clearInterval(timerInterval);
+
     // Empty Question and Selection Choice Div
     clear();
     
@@ -255,6 +313,7 @@ function endGame() {
     scoreGame();
     // Send user to form to save username and score
     saveUser();
+
 }
 
 // ---------------------------------------------------- //
@@ -271,6 +330,61 @@ function scoreGame() {
     return;
 }
 
+
+// ---------------------------------------------------- //
+//
+// Save User/Score Function: 
+//
+// ---------------------------------------------------- //
+function saveUser() {
+
+    // Un-Hide User form and score
+    userForm.classList.remove("hide");
+
+    // Update user score in DOM
+    let userScore = document.getElementById("userScore");
+    userScore.innerHTML = gameScore;
+
+    // Grab form input element 
+    let userInitials = document.getElementById("userInitials");
+    // Clear form input field
+    userInitials.value = '';
+
+    // Capture Submit Event
+    let userSubmit = document.getElementById("userSubmit");
+    userSubmit.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        console.log(userInitials.value);
+
+        let tempArray = localStorage.getItem("userScores");
+        let parsedTempArray = JSON.parse(tempArray);
+        console.log(parsedTempArray);
+        
+        // Add current game score to high score array
+        parsedTempArray.push( 
+            {   
+                username: userInitials.value, 
+                score: gameScore 
+            } 
+            ); 
+            
+        console.log(parsedTempArray);
+
+        // Save updated JavaScript OBJECT to local storage by turning it into a JSON OBJECT
+        localStorage.setItem('userScores', JSON.stringify(parsedTempArray));
+
+        // Clear form input
+        userInitials.innerHTML = '';
+
+        // Show Leader Board
+        showLeader();
+        // Un-Hide start button
+        btn.classList.remove("hide");
+    });
+}
+
+
 // ---------------------------------------------------- //
 //
 // Leader Board Function: Function will pull userScore OBJECT
@@ -286,68 +400,20 @@ function showLeader() {
     // Un-hide score board div
     scoreBoard.classList.remove("hide");
 
-    let highScoreBoard = document.getElementById("highscores");
-    // TEST if any scores currently exist
-    if(!highScoreArray) {
-        highScoreBoard.textContent = " - "
-    }
+    let scoreBoardText = document.getElementById("highscores");
+    console.log("parseing local storage object")
+    let highScoreBoard = localStorage.getItem('userScores');
+    let parsedScoreBoard = JSON.parse(highScoreBoard);
+
+    console.log(parsedScoreBoard);
+
     // Display high scores
-    for(let i = 0; i < highScoreArray.length; i++) {
+    for (let i = 0; i < parsedScoreBoard.length; i++) {
         let newScore = document.createElement("li");
-        newScore.textContent = highScoreArray[i].username + " : " + highScoreArray[i].score;
-        highScoreBoard.appendChild(newScore);
+        newScore.textContent = parsedScoreBoard[i].username + " : " + parsedScoreBoard[i].score;
+        scoreBoardText.appendChild(newScore);
     }
 
 }
 
-// ---------------------------------------------------- //
-//
-// Save User/Score Function:
-//
-// ---------------------------------------------------- //
-function saveUser() {
-    // Un-Hide User form and score
-    userForm.classList.remove("hide");
 
-    // Update user score in DOM
-    let userScore = document.getElementById("userScore");
-    userScore.innerHTML = gameScore;
-
-    // Grab form input element 
-    let userInitials = document.getElementById("userInitials");
-
-    // Capture Submit Event
-    let userSubmit = document.getElementById("userSubmit");
-    userSubmit.addEventListener("click", function(event) {
-        event.preventDefault();
-
-        console.log(userInitials.value);
-
-        // Add current game score to high score array
-        highScoreArray.push( 
-            {   
-                username: userInitials.value, 
-                score: gameScore 
-            } 
-        ); 
-
-        // Create a new object to hold the new score and name
-        let highUserScore = {
-            username: userInitials.value,
-            score: gameScore
-        } 
-        // Add new submission to Local Storage
-        localStorage.setItem("userScore", JSON.stringify(highUserScore));
-
-        // Clear form input
-        userInitials.innerHTML = '';
-
-        // Show Leader Board
-        showLeader();
-    });
-    
-}
-
-
-// Initalize Variables on Browser Load
-initialize();
