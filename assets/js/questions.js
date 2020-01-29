10daf3f99aeded920d5dc574a404250c302b6d8a
ehoversten <ehoversten@gmail.com>
2020-01-29 10:06:43 -0500

    adding real questions to questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,33 +1,44 @@
 // Question Array 
-var  questions = [
-    {
-        title: "Some where over the _____",
-        choices: ["rainbow", "fence", "bush", "next hill"],
-        answer: "rainbow"
-    },
-    {
-        title: "Rick and ______",
-        choices: ["Squirl", "Morty", "Bobby", "Rick"],
-        answer: "Morty"
-    },
-    {
-        title: "Iron Man",
-        choices: ["sucks", "is awesome", "Black Sabbath", "Tony Stark"],
-        answer: "Black Sabbath"
-    },
-    {
-        title: "Which are not guitar manufactures?",
-        choices: ["Fender", "Gibson", "Pearl", "Rickenbacker"],
-        answer: "Pearl"
-    },
-    {
-        title: "Star Wars",
-        choices: ["Yoda", "Han Solo", "Darth Vader", "Chewie"],
-        answer: "Chewie"
-    },
-    {
-        title: "Road Trip",
-        choices: ["Nashville", "Seattle", "Kansas", "Savannah"],
-        answer: "Kansas"
-    },
+var questions = [
+  {
+    title: "Some where over the _____",
+    choices: ["rainbow", "fence", "bush", "next hill"],
+    answer: "rainbow"
+  },
+  {
+    title: "Rick and ______",
+    choices: ["Squirl", "Morty", "Bobby", "Rick"],
+    answer: "Morty"
+  },
+  {
+    title: "What is the correct JavaScript syntax to write 'Hello World'?",
+    choices: [
+      "System.out.println('Hello World')",
+      "println ('Hello World')",
+      "document.write('Hello World')",
+      "response.write('Hello World')"
+    ],
+    answer: "document.write('Hello World')"
+  },
+  {
+    title:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choices: [
+      "<script>",
+      "<script src='xxx.js'>",
+      "<script href='xxx.js'>",
+      "<script name='xxx.js>"
+    ],
+    answer: "<script src='xxx.js'>"
+  },
+  {
+    title: "Which of the following are NOT considered FALSY in JavaScript?",
+    choices: ["null", "[]", "undefined", "false"],
+    answer: "[]"
+  },
+  {
+    title: "What is the output of the given expression: '2' === 2 ?",
+    choices: ["true", "false"],
+    answer: "false"
+  }
 ];
